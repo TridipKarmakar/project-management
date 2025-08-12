48ea17db56c2c3ea8848d38afba7951bc45c6c96
Tridip Karmakar <karmakartridip97@gmail.com>
2025-08-12 17:04:47 +0000

    understanding the dotenv

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Start of Backend Project");
+import dotenv from "dotenv";
+
+
+dotenv.config({
+    path: "./.env"
+})
+
+let myusername = process.env.database
+console.log("My user name : ",myusername);
+
+console.log("Start of the Backend Project");
