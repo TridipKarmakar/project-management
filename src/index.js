ffb3fde3210fc8c108fedcdc7107aff35dfa20ae
Tridip Karmakar <karmakartridip97@gmail.com>
2025-08-15 11:33:56 +0000

    understand the db connection, api error, api response, and other things

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
+
 dotenv.config({
     path: "./.env"
 })
 
 const port = process.env.PORT || 3000
 
+connectDB()
+  .then(() => {
+      app.listen(port, () => {
+        console.log(`Example app listening on port https://probable-palm-tree-9r6774gp4w73rg7-${port}.app.github.dev/`)
+    })  
 
+  })
+  .catch((err)=> {
+    console.log("Mongodb connection error",err);
+    process.exit(1)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port https://probable-palm-tree-9r6774gp4w73rg7-${port}.app.github.dev/`)
-})
+  })
