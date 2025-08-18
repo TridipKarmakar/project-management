006933a4f9ea178b9c03b8bf922fbaeb628eca86
Tridip Karmakar <karmakartridip97@gmail.com>
2025-08-18 18:54:39 +0000

    update mail gen

--- src/models/user-models.js
+++ src/models/user-models.js
@@ -1,5 +1,7 @@
 import mongoose, { mongo, Schema } from "mongoose";
 import  bcrypt  from "bcrypt";
+import jwt, { sign } from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = new Schema(
     {
@@ -71,5 +73,42 @@ userSchema.pre("save", async function(next) {
 userSchema.methods.isPasswordCorrect = async function (password) {
      return await bcrypt.compare(password, this.password)
 }
+
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRATE,
+        {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRATE,
+        {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+    )
+}
+
+
+userSchema.methods.generateTemporaryToken = function () {
+    const unHashedToken = crypto.randomBytes(20).toString("hex")   
+
+    const hashedToken = crypto
+        .createHash("sha256")
+        .update(unHashedToken)
+        .digest("hex")
+        
+    const tokenExpiry = Date.now() + (20*60*1000)
+    return { unHashedToken, hashedToken, tokenExpiry }
+}
+
 
 export const User = mongoose.model("User", userSchema)
---
+++ src/utiles/mail.js
@@ -0,0 +1,82 @@
+
+import Mailgen from "mailgen";
+import nodemailer from "nodemailer"
+
+
+const sendEmail = async (option) => {
+    const mailGenerator = new Mailgen(
+        {
+            theme: "Default",
+            product: {
+                name: "Task Manager",
+                link: "https://taskmanagerlink.com"
+            }
+        }
+    )
+
+    const emailTextual = mailGenerator.generatePlaintext(option.mailgenContent)
+    const emailHtml = mailGenerator.generate(option.mailgenContent)
+
+    const transpoter = nodemailer.createTransport({
+        host: process.env.MAILTRAP_SMTP_HOST,
+        port: process.env.MAILTRAP_SMTP_PORT,
+        auth: {
+            user: MAILTRAP_SMTP_USER,
+            pass: MAILTRAP_SMTP_PASS
+        }
+    })
+
+    const mail = {
+        from: "mail.taskmanager@example.com",
+        to: option.email,
+        subject: option.subject,
+        text: emailTextual,
+        html: emailHtml
+    }
+
+    try {
+        await transpoter.sendMail(mail)
+    } catch (error) {
+        console.error("Email service failed siliently. Make sure that you have provided your MailTrap credentials in the .env file");
+        console.error("Error: ",error);
+    }
+
+}
+
+const emailVerificationMailgenContent = (username, verficationUrl) => {
+    return {
+        body: {
+            name: username,
+            intro: "Welcome to our App! we're excited to have you on board.",
+            action:{
+                instructions: "To verify your email please click on the following button",
+                button: {
+                    color: "#22BC66",
+                    text: "Verify your email",
+                    link: verficationUrl
+                }
+            },
+            outro: "Need hel, or have questions? Just reply to this mail, we'd love to help."
+        }
+    }
+}
+const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
+    return {
+        body: {
+            name: username,
+            intro: "We gota requet to reset the password of your account.",
+            action:{
+                instructions: "To reset your password click on the following button or link",
+                button: {
+                    color: "#22BC66",
+                    text: "Reset Password",
+                    link: passwordResetUrl
+                }
+            },
+            outro: "Need help, or have questions? Just reply to this mail, we'd love to help."
+        }
+    }
+
+}
+
+export { emailVerificationMailgenContent, forgotPasswordMailgenContent,sendEmail }
